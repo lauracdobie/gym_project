a6ea8e4332d54a4619b12f70ec164cace7d28ee0
Laura Dobie <moving.toyshop@gmail.com>
2020-11-28 13:45:57 +0000

    Booking set up correctly

--- run_tests.py
+++ run_tests.py
@@ -2,6 +2,7 @@ import unittest
 from tests.member_test import *
 from tests.class_type_test import *
 from tests.fitness_class_test import *
+from tests.booking_test import *
 
 if __name__ == "__main__":
     unittest.main()
---
+++ tests/booking_test.py
@@ -0,0 +1,17 @@
+import unittest
+from models.fitness_class import FitnessClass
+from models.class_type import ClassType
+from models.member import Member
+from models.booking import Booking
+
+class TestBookingClass(unittest.TestCase):
+    def setUp(self):
+        self.class_type = self.body_pump = ClassType("Body Pump", "Barbell workout to music. Great for building strength and lean muscle!", "High", "Moderate")
+        self.body_pump_express = FitnessClass(self.class_type, "03/12/2020", "14:00", "30 minutes", "Charlotte Anderson", 20, "Studio 1")
+        self.claire = Member("Claire Whittaker", "10 Smith Street", "07783902109", True, 1004)
+        self.booking = Booking(self.claire, self.body_pump_express)
+
+    def test_booking_has_all_details(self):
+        self.assertEqual("Claire Whittaker", self.booking.member.name)
+        self.assertEqual("Body Pump", self.booking.fitness_class.class_type.name)
+
