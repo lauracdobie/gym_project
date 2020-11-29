b705b081e62697a30cc11c862d667f2b51746b78
Laura Dobie <moving.toyshop@gmail.com>
2020-11-29 14:33:59 +0000

    Bit of a commit of woe. All my functions are working for all the repositories EXCEPT the edit ones, which delete the id's from the records once they update them. Urgh

--- console.py
+++ console.py
@@ -9,6 +9,7 @@ import repositories.class_type_repository as class_type_repository
 import repositories.fitness_class_repository as fitness_class_repository
 import repositories.booking_repository as booking_repository
 
+booking_repository.delete_all()
 member_repository.delete_all()
 class_type_repository.delete_all()
 fitness_class_repository.delete_all()
@@ -19,11 +20,20 @@ member_repository.add(claire)
 angela = Member("Angela O'Shaughnessy", "12 Smith Street", "07785976809", "angelaO79@gmail.com", False, 895)
 member_repository.add(angela)
 
+# angela = Member("Angela O'Shaughnessy", "14 Smith Street", "07788976224", "angelaO55@gmail.com", False, 895)
+# member_repository.edit(angela)
+
 body_pump = ClassType("Body Pump", "Barbell workout to music. Great for building strength and lean muscle!", "High", "Moderate")
 class_type_repository.add(body_pump)
 
 body_balance = ClassType("Body Balance", "A mix of yoga, tai chi and pilates. Great for mobility", "Low", "Low")
 class_type_repository.add(body_balance)
+
+spin = ClassType("Spin", "Calorie-burning workout to music on an exercise bike. Gets your heart rate up!", "High", "Medium")
+class_type_repository.add(spin)
+
+# spin = ClassType("Spin", "Energetic cycle workout to music.", "Medium", "Low")
+# class_type_repository.edit(spin)
 
 body_pump_express = FitnessClass(body_pump, "03/12/2020", "14:00", "30 minutes", "Charlotte Anderson", 20, "Studio 1")
 fitness_class_repository.add(body_pump_express)
@@ -31,12 +41,19 @@ fitness_class_repository.add(body_pump_express)
 body_balance_am = FitnessClass(body_balance, "04/12/2020", "10:00", "55 minutes", "Madeleine Johnson", 8, "Studio 2")
 fitness_class_repository.add(body_balance_am)
 
-body_balance_am = FitnessClass(body_balance, "05/12/2020", "18:00", "45 minutes", "Anna Murray", 15, "Studio 1")
-fitness_class_repository.edit(body_balance_am)
+# body_balance_am = FitnessClass(body_balance, "05/12/2020", "18:00", "45 minutes", "Anna Murray", 15, "Studio 1")
+# fitness_class_repository.edit(body_balance_am)
 
 booking_1 = Booking(angela, body_pump_express)
 booking_repository.add(booking_1)
 
-booking_2 = Booking(claire, body_balance_am)
+# booking_2 = Booking(claire, body_balance_am)
+# booking_repository.add(booking_2)
+
+# booking_2 = Booking(angela, body_balance_am)
+# booking_repository.edit(booking_2)
+
+booking_3 = Booking(claire, body_pump_express)
+booking_repository.add(booking_3)
 
 pdb.set_trace()
--- repositories/booking_repository.py
+++ repositories/booking_repository.py
@@ -3,6 +3,10 @@ from models.fitness_class import FitnessClass
 from models.class_type import ClassType
 from models.member import Member
 from models.booking import Booking
+import repositories.member_repository as member_repository
+import repositories.class_type_repository as class_type_repository
+import repositories.fitness_class_repository as fitness_class_repository
+import pdb
 
 #delete all
 def delete_all():
@@ -16,14 +20,42 @@ def delete(id):
     run_sql(sql, value)
 
 #add
-
 def add(booking):
     sql = "INSERT INTO bookings (member_id, fitness_class_id) VALUES (%s, %s) RETURNING id"
     values = [booking.member.id, booking.fitness_class.id]
-    result = run_sql(sql, values)
-    id = result[0]['id']
+    results = run_sql(sql, values)
+    id = results[0]['id']
     booking.id = id
+    return booking.id
+ 
 
 #edit
+def edit(booking):
+    sql = "UPDATE bookings SET (member_id, fitness_class_id) = (%s, %s) WHERE id = %s"
+    values = [booking.member.id, booking.fitness_class.id, booking.id]
+    # pdb.set_trace()
+    run_sql(sql, values)
+
 #select all
+def select_all():
+    sql = "SELECT * FROM bookings"
+    results = run_sql(sql)
+    all_bookings = []
+
+    for result in results:
+        member = member_repository.select(result['member_id'])
+        fitness_class = fitness_class_repository.select(result['fitness_class_id'])
+        booking = Booking(member, fitness_class, result['id'])
+        all_bookings.append(booking)
+    
+    return all_bookings
+
 #select
+def select(id):
+    sql = "SELECT * FROM bookings WHERE id = %s"
+    value = [id]
+    result = run_sql(sql, value)[0]
+    member = member_repository.select(result['member_id'])
+    fitness_class = fitness_class_repository.select(result['fitness_class_id'])
+    booking = Booking(member, fitness_class, result['id'])
+    return booking
