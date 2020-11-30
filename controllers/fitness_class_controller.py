87bf1c6bb1f7f53067de5ccaa8343f012d2eec76
Laura Dobie <moving.toyshop@gmail.com>
2020-11-30 12:35:28 +0000

    Add member html form complete

--- controllers/fitness_class_controller.py
+++ controllers/fitness_class_controller.py
@@ -9,6 +9,4 @@ fitness_classes_blueprint = Blueprint("fitness_classes", __name__)
 @fitness_classes_blueprint.route("/fitness-classes")
 def upcoming_classes():
     fitness_classes = fitness_class_repository.select_all()
-    # for class in fitness_classes:
-    #     class_type = fitness_class.class_type.name
     return render_template("fitness_classes/index.html", fitness_classes = fitness_classes)
