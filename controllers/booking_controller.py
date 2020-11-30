52e56e507b3825315317a68adcf5db4d2adcf152
Laura Dobie <moving.toyshop@gmail.com>
2020-11-30 16:43:37 +0000

    Booking a class works - woo hoo

--- controllers/booking_controller.py
+++ controllers/booking_controller.py
@@ -5,25 +5,26 @@ from models.class_type import ClassType
 from models.member import Member
 from models.booking import Booking
 import repositories.booking_repository as booking_repository
+import repositories.fitness_class_repository as fitness_class_repository
+import repositories.member_repository as member_repository
 
 bookings_blueprint = Blueprint("bookings", __name__)
 
-@bookings_blueprint.route("/book-class")
+@bookings_blueprint.route("/bookings")
+def all_bookings():
+    all_bookings = booking_repository.select_all()
+    return render_template("bookings/all_bookings.html", all_bookings = all_bookings)
+
+@bookings_blueprint.route("/bookings/book-class")
 def booking_form():
-    return render_template("bookings/index.html")
+    all_fitness_classes = fitness_class_repository.select_all()
+    return render_template("/bookings/index.html", all_fitness_classes = all_fitness_classes)
 
-# @bookings_blueprint.route("/book-class", methods=['POST'])
-# def create_booking():
-#     fitness_class = fitness_class_repository.select(request.form['fitness_class_id'])
-#     date = request.form['date']
-#     time = request.form['time']
-#     duration = request.form['duration']
-#     instructor = request.form['instructor']
-#     location = request.form['location']
-#     capacity = request.form['capacity']
-
-#     print("🐰" + class_type.name + "🐰")
-
-#     new_class = FitnessClass(class_type, date, time, duration, instructor, capacity, location)
-#     fitness_class_repository.add(new_class)
-#     return redirect('/fitness-classes')
+@bookings_blueprint.route("/bookings", methods=['POST'])
+def create_booking():
+    fitness_class = fitness_class_repository.select(request.form['fitness_class_id'])
+    member = member_repository.select_by_membership_no(request.form['membership_no'])
+    
+    new_booking = Booking(member, fitness_class)
+    booking_repository.add(new_booking)
+    return redirect('/bookings')
