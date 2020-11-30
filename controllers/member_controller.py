d8ec70402dd916f9c04bf9290c03354703e66813
Laura Dobie <moving.toyshop@gmail.com>
2020-11-30 11:57:23 +0000

    Member list has actual members

---
+++ controllers/member_controller.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.fitness_class import FitnessClass
+from models.class_type import ClassType
+from models.member import Member
+from models.booking import Booking
+import repositories.member_repository as member_repository
+
+members_blueprint = Blueprint("members", __name__)
+
+@members_blueprint.route("/members")
+def member_list():
+    all_members = member_repository.select_all()
+    return render_template("members/index.html", all_members = all_members)
