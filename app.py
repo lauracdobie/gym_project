f8a59ec5001a700dae448c05a43daaf622632ca5
Laura Dobie <moving.toyshop@gmail.com>
2020-11-30 19:36:31 +0000

    Can add and edit class types

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 from controllers.member_controller import members_blueprint
 from controllers.fitness_class_controller import fitness_classes_blueprint
-# from controllers.class_type_controller import class_types_blueprint
+from controllers.class_type_controller import class_types_blueprint
 from controllers.booking_controller import bookings_blueprint
 
 app = Flask(__name__)
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(members_blueprint)
 app.register_blueprint(fitness_classes_blueprint)
 app.register_blueprint(bookings_blueprint)
+app.register_blueprint(class_types_blueprint)
 
 @app.route('/')
 def home():
---
+++ controllers/class_type_controller.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.class_type import ClassType
+import repositories.class_type_repository as class_type_repository
+
+class_types_blueprint = Blueprint("class_types", __name__)
+
+@class_types_blueprint.route("/class-types")
+def class_type_list():
+    class_types = class_type_repository.select_all()
+    for class_type in class_types:
+        print("🐰" + class_type.name + "🐰")
+    return render_template("class_types/index.html", class_types=class_types)
+
+@class_types_blueprint.route("/class-types/add", methods=['GET'])
+def new_class_type():
+    return render_template("class_types/add.html")
+
+@class_types_blueprint.route("/class-types", methods=['POST'])
+def create_class_type():
+    name = request.form['name']
+    description = request.form['description']
+    intensity = request.form['intensity']
+    difficulty = request.form['difficulty']
+
+    new_class_type = ClassType(name, description, intensity, difficulty)
+    class_type_repository.add(new_class_type)
+    return redirect('/class-types')
+
+@class_types_blueprint.route("/class-types/<id>/edit", methods=['GET'])
+def edit_class_type_details(id):
+    class_type = class_type_repository.select(id)
+    return render_template("class_types/edit.html", class_type = class_type)
+
+@class_types_blueprint.route("/class-types/<id>", methods=['POST'])
+def update_class_type_details(id):
+    name = request.form['name']
+    description = request.form['description']
+    intensity = request.form['intensity']
+    difficulty = request.form['difficulty']
+
+    updated_class_type = ClassType(name, description, intensity, difficulty, id)
+    class_type_repository.edit(updated_class_type)
+    return redirect('/class-types')
