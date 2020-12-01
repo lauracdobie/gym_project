fb7d2403cb12041fb29aba0c5c105ec497ca2383
Laura Dobie <moving.toyshop@gmail.com>
2020-12-01 10:56:01 +0000

    Capacity check works, but I need to change delete booking so that it updates the capacity when someone cancels their booking.

--- controllers/booking_controller.py
+++ controllers/booking_controller.py
@@ -20,14 +20,42 @@ def booking_form():
     all_fitness_classes = fitness_class_repository.select_all()
     return render_template("/bookings/index.html", all_fitness_classes = all_fitness_classes)
 
+# @bookings_blueprint.route("/bookings", methods=['POST'])
+# def create_booking():
+#     fitness_class = fitness_class_repository.select(request.form['fitness_class_id'])
+#     member = member_repository.select_by_membership_no(request.form['membership_no'])
+    
+#     new_booking = Booking(member, fitness_class)
+#     booking_repository.add(new_booking)
+#     return redirect('/bookings')
+
+@bookings_blueprint.route("/bookings/class-full")
+def class_full_message():
+    return render_template("bookings/class_full.html")
+
 @bookings_blueprint.route("/bookings", methods=['POST'])
 def create_booking():
     fitness_class = fitness_class_repository.select(request.form['fitness_class_id'])
     member = member_repository.select_by_membership_no(request.form['membership_no'])
-    
-    new_booking = Booking(member, fitness_class)
-    booking_repository.add(new_booking)
-    return redirect('/bookings')
+    id = request.form['fitness_class_id']
+    class_type = fitness_class.class_type
+    date = fitness_class.date
+    time = fitness_class.time
+    duration = fitness_class.duration
+    instructor = fitness_class.instructor
+    location = fitness_class.location
+
+
+    if fitness_class.capacity > 0:
+        capacity = fitness_class.capacity - 1
+        fitness_class = FitnessClass(class_type, date, time, duration, instructor, capacity, location, id)
+        fitness_class_repository.edit(fitness_class)
+        new_booking = Booking(member, fitness_class)
+        booking_repository.add(new_booking)
+        return redirect('/bookings')
+
+    else:
+        return redirect('/bookings/class-full')
 
 @bookings_blueprint.route("/bookings/<id>/delete", methods=['POST'])
 def delete_booking(id):
